90f039aeed4cefe4bcf8b5e595c71f5bf7f44940
Volodymyr Shorinov <volodymyr.shorinov@emotorwerks.com>
2021-03-17 14:04:00 +0200

    Do not load User when navigated from list

--- src/app/store/effects.ts
+++ src/app/store/effects.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { exhaustMap, filter, map, mergeMap, tap, withLatestFrom } from 'rxjs/operators';
-import { Store } from '@ngrx/store';
+import { Action, Store } from '@ngrx/store';
 import { Actions, createEffect, ofType } from '@ngrx/effects'
 import { UsersState } from './reducer';
 import { ApiGetIUserDetail, ApiGetIUsers, ApiGetIUsersSuccess, ApiGetIUserDetailSuccess } from './actions';
 import { UserServiceService } from '../services/user-service.service';
-import { getMergedRoute } from './selectors';
+import { getMergedRoute, userState } from './selectors';
 import { UserRouterState } from '../models/user-router-state.model';
+import { User } from '../models/user.model';
+import { Observable, of } from 'rxjs';
 
 @Injectable()
 export class UserEffects {
@@ -21,18 +23,34 @@ export class UserEffects {
 
   loadUserDetail$ = createEffect(() => this.actions$.pipe(
     ofType(ApiGetIUserDetail()),
-    withLatestFrom(this.store.select(getMergedRoute)),
-    map(([, routerState]: [any, UserRouterState]) => routerState.params['id']),
-    mergeMap((userId) => this.userService.getUserDetail(userId).pipe(
-      map((userDetail) => ApiGetIUserDetailSuccess()({userDetail}))
-    ))
+    withLatestFrom(this.store),
+    mergeMap(([, state]: [any, { userState: UsersState, router: { state: UserRouterState } }]) => {
+      const { userState, router } = state;
+      const requestedUserId: number = +router.state.params['id'];
+      const existedInStoreUser: User = findRequestedUserExistsInStore(userState, requestedUserId);
+      if (existedInStoreUser) {
+        return of(existedInStoreUser).pipe(map((userDetail) => ApiGetIUserDetailSuccess()({userDetail})));
+      } else {
+        return this.userService.getUserDetail(requestedUserId).pipe(
+          map((userDetail) => ApiGetIUserDetailSuccess()({userDetail}))
+        );
+      }
+    })
   )
 );
 
   constructor(
     private actions$: Actions,
-    private store: Store<UsersState>,
+    private store: Store<{userState: UsersState; router: { state: UserRouterState }}>,
     private userService: UserServiceService
   ) {}
 }
 
+function findRequestedUserExistsInStore(userState: UsersState, userId: number): User | null {
+  if (userState.users && userState.users) {
+    const foundUser: User = userState.users.find(u => u.id === userId);
+    return foundUser ? foundUser : null
+  } else {
+    return null;
+  }
+}
